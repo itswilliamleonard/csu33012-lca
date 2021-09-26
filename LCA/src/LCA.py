575f8eecc3a50ff130a0190e21ecf5cb0a831dc1
liam <lolionai@tcd.ie>
2021-09-26 20:53:06 +0100

    Add LCA.py (using command line)

---
+++ LCA/src/LCA.py
@@ -0,0 +1,12 @@
+# PYTHON 3.9x
+# LOWEST COMMON ANCESTOR
+# LIAM O LIONAIRD [19335530]
+# 25/09/21
+
+class Node:
+    def __init__():
+        self.left = Node()
+        self.right = Node()
+
+def lowestCommonAncestor(node1, node2, root):
+    # TODO
